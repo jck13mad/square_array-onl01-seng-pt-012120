5add2dbb42b2cc4882eae16fd7806879e4c21e9a
Jack Madden <jck13mad@gmail.com>
2020-01-26 18:12:04 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,6 +2,7 @@ def square_array(array)
   # your code here
   
   new_array = []
-  array.each{|a| new_array.push(a*a)}
-  return new_array
+  array.each do |a| 
+    new_array.push(a*a)}
+    return new_array
 end
